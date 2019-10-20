cc5fe1a357141b699be1e08b0b5354d5d9f14bd9
Karolina Wanat <carolinawanat@gmail.com>
2019-10-20 13:26:19 +0100

    vaadin added

--- src/main/java/com/uk/jobfinder/JobController.java
+++ src/main/java/com/uk/jobfinder/JobController.java
@@ -1,31 +1,44 @@
 package com.uk.jobfinder;
 
 import com.uk.jobfinder.model.Result;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 
 import java.util.List;
 
-@Controller
 @Route("show-all")
-public class JobController extends HorizontalLayout {
+public class JobController extends VerticalLayout {
 
     JobProvider jobProvider;
 
     @Autowired
     public JobController(JobProvider jobProvider) {
         this.jobProvider = jobProvider;
-    }
 
-    public void showMoviesPage() {
+        ComboBox<String> comboBoxCity = new ComboBox<String>("City");
+        comboBoxCity.setItems("London", "Birmingham", "Manchester", "Glasgow", "Newcastle", "Sheffield", "Liverpool", "Leeds");
+
+        ComboBox<String> comboBoxJob = new ComboBox<String>("Find");
+        comboBoxJob.setItems("Java", "Junior Java", "Senior Java");
+
+        Button buttonSubmit = new Button("Submit", new Icon(VaadinIcon.SELECT));
+        buttonSubmit.setIconAfterText(true);
+
+
         List<Result> jobs = jobProvider.getJobs();
+        Grid<Result> grid = new Grid<>(Result.class);
+        grid.setItems(jobs);
+        grid.setColumns("employerName", "jobTitle", "locationName", "minimumSalary",
+                "maximumSalary", "currency", "jobDescription", "applications", "jobUrl");
 
-
+        add(comboBoxCity, comboBoxJob,buttonSubmit, grid);
 
     }
-
 
 }
