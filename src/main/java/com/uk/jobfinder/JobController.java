3b4f70c0df89180f12dc5a73a3f2dce2305450c5
Karolina Wanat <carolinawanat@gmail.com>
2019-10-20 11:59:35 +0100

    api added

--- src/main/java/com/uk/jobfinder/JobController.java
+++ src/main/java/com/uk/jobfinder/JobController.java
@@ -2,15 +2,15 @@ package com.uk.jobfinder;
 
 import com.uk.jobfinder.model.Result;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@Route("show-all")
 public class JobController extends HorizontalLayout {
 
     JobProvider jobProvider;
@@ -20,14 +20,11 @@ public class JobController extends HorizontalLayout {
         this.jobProvider = jobProvider;
     }
 
-    @GetMapping
-    @RequestMapping("/showJobs")
-    public String showMoviesPage(Model model) {
+    public void showMoviesPage() {
         List<Result> jobs = jobProvider.getJobs();
-        model.addAttribute("jobs", jobs);
 
 
-        return "showJobs";
+
     }
 
 
