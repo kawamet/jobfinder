a36123a027d98e9e0ca4027c2140243d662d774d
Karolina Wanat <carolinawanat@gmail.com>
2019-11-04 21:16:31 +0000

    calculation/ count technologies

--- src/main/java/com/uk/jobfinder/JobApi.java
+++ src/main/java/com/uk/jobfinder/JobApi.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 public class JobApi {
@@ -21,13 +22,32 @@ public class JobApi {
     @GetMapping("api/jobs")
     public List<Result> getJobApi(@RequestParam String city,
                                   @RequestParam String keywords,
-                                  @RequestParam(value = "date", required = false) String date){
+                                  @RequestParam(value = "date", required = false) String date) {
         List<Result> jobs = jobProvider.getJobs(keywords, city);
         if (date == null) {
             return jobs;
-        }else {
+        } else {
             List<Result> collect = jobs.stream().filter(e -> e.getDate().equals(date)).collect(Collectors.toList());
+
             return collect;
         }
     }
+
+    @GetMapping("api/techsum")
+    public Map<String, Long> getJobApiCount(@RequestParam String city,
+                                            @RequestParam String keywords) {
+        List<Result> jobs = jobProvider.getJobs(keywords, city);
+        List<String> technologies = Arrays.asList("spring", "sql", "microservices", "mongodb", "aws", "react", "hibernate", "mysql", "apis", "git", "mokito", "junit", "tdd", "html", "css", "javascript", "bootstrap", "docker", "jenkins", "ci/cd", "design paterns", "jsp", "servlets", "tomcat");
+        Map<String, Long> countTech = new HashMap<>();
+
+        for (String technology : technologies) {
+            Long count = jobs.stream().filter(e -> e.getJobDescription().toLowerCase().contains(technology)).count();
+            countTech.put(technology, count);
+        }
+        Map<String, Long> collect = countTech.entrySet().stream().filter(e -> e.getValue() > 0).collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
+
+        return collect;
+    }
+
+
 }
