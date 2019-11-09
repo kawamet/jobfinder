39622b030e591a30cb3b80625d75c26733409d39
bart7801 <bart7801@o2.pl>
2019-11-09 09:26:26 +0000

    deduction the period of sent emails & information about adding an email to the database after clicking SAVE, resources

--- src/main/java/com/uk/jobfinder/email/AddEmail.java
+++ src/main/java/com/uk/jobfinder/email/AddEmail.java
@@ -5,6 +5,7 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
@@ -37,53 +38,53 @@ public class AddEmail extends VerticalLayout {
         comboBoxJobPosition = new ComboBox<>("Select job position");
         comboBoxJobPosition.setItems("Senior Java", "Java", "Junior Java", "Graduate Java", "Academy Java");
 
-        DatePicker startDatePicker = new DatePicker();
-        startDatePicker.setLabel("Start");
-        startDatePicker.setValue(LocalDate.now());
-        DatePicker endDatePicker = new DatePicker();
-        endDatePicker.setLabel("End");
-        endDatePicker.setValue(LocalDate.now());
-
-        startDatePicker.addValueChangeListener(event -> {
-            LocalDate selectedDate = event.getValue();
-            LocalDate endDate = endDatePicker.getValue();
-            if (selectedDate != null) {
-                endDatePicker.setMin(selectedDate.plusDays(1));
-                if (endDate == null) {
-                    endDatePicker.setOpened(true);
-                    message.setText("Select the ending date");
-                } else {
-                    message.setText(
-                            "Selected period:\nFrom " + selectedDate.toString()
-                                    + " to " + endDate.toString());
-                }
-            } else {
-                endDatePicker.setMin(null);
-                message.setText("Select the starting date");
-            }
-        });
-
-        endDatePicker.addValueChangeListener(event -> {
-            LocalDate selectedDate = event.getValue();
-            LocalDate startDate = startDatePicker.getValue();
-            if (selectedDate != null) {
-                startDatePicker.setMax(selectedDate.minusDays(1));
-                if (startDate != null) {
-                    message.setText(
-                            "Selected period:\nFrom " + startDate.toString()
-                                    + " to " + selectedDate.toString());
-                } else {
-                    message.setText("Select the starting date");
-                }
-            } else {
-                startDatePicker.setMax(null);
-                if (startDate != null) {
-                    message.setText("Select the ending date");
-                } else {
-                    message.setText("No date is selected");
-                }
-            }
-        });
+//        DatePicker startDatePicker = new DatePicker();
+//        startDatePicker.setLabel("Start");
+//        startDatePicker.setValue(LocalDate.now());
+//        DatePicker endDatePicker = new DatePicker();
+//        endDatePicker.setLabel("End");
+//        endDatePicker.setValue(LocalDate.now());
+//
+//        startDatePicker.addValueChangeListener(event -> {
+//            LocalDate selectedDate = event.getValue();
+//            LocalDate endDate = endDatePicker.getValue();
+//            if (selectedDate != null) {
+//                endDatePicker.setMin(selectedDate.plusDays(1));
+//                if (endDate == null) {
+//                    endDatePicker.setOpened(true);
+//                    message.setText("Select the ending date");
+//                } else {
+//                    message.setText(
+//                            "Selected period:\nFrom " + selectedDate.toString()
+//                                    + " to " + endDate.toString());
+//                }
+//            } else {
+//                endDatePicker.setMin(null);
+//                message.setText("Select the starting date");
+//            }
+//        });
+//
+//        endDatePicker.addValueChangeListener(event -> {
+//            LocalDate selectedDate = event.getValue();
+//            LocalDate startDate = startDatePicker.getValue();
+//            if (selectedDate != null) {
+//                startDatePicker.setMax(selectedDate.minusDays(1));
+//                if (startDate != null) {
+//                    message.setText(
+//                            "Selected period:\nFrom " + startDate.toString()
+//                                    + " to " + selectedDate.toString());
+//                } else {
+//                    message.setText("Select the starting date");
+//                }
+//            } else {
+//                startDatePicker.setMax(null);
+//                if (startDate != null) {
+//                    message.setText("Select the ending date");
+//                } else {
+//                    message.setText("No date is selected");
+//                }
+//            }
+//        });
 
         Button button = new Button("Save!");
 
